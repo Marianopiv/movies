2579c796f511d21c5d53e6e387b9d96f6756777d
Marianopiv <marianopividori93@gmail.com>
2023-03-21 22:13:52 -0300

    agrego mas cosas

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ import { defineConfig, loadEnv } from 'vite';
 /** @type {import('vite').UserConfig} */
 export default ({ mode }) => {
     // Extends 'process.env.*' with VITE_*-variables from '.env.(mode=production|development)'
-    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
-    return defineConfig({
+/*     process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+ */    return defineConfig({
       plugins: [react()],
     }); 
 };
