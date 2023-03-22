20c258dc752b86b4d5488906d7f3e5c39cbe4a4e
Marianopiv <marianopividori93@gmail.com>
2023-03-21 21:37:43 -0300

    ajusto envs

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env':{}
-  }
-})
+import { defineConfig, loadEnv } from 'vite';
+
+/** @type {import('vite').UserConfig} */
+export default ({ mode }) => {
+    // Extends 'process.env.*' with VITE_*-variables from '.env.(mode=production|development)'
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+    return defineConfig({
+      plugins: [react()],
+    }); 
+};
