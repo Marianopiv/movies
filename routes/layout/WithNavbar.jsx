e7f36b80f13fc8e70c1ef0e3bcfcc46e9237de38
Marianopiv <marianopividori93@gmail.com>
2023-03-21 23:48:15 -0300

    adjust bg

--- routes/layout/WithNavbar.jsx
+++ routes/layout/WithNavbar.jsx
@@ -7,10 +7,30 @@ import { getImg } from "../../src/helper";
 const WithNavbar = () => {
   const { setToogleMovie, toogleMovie, featured } = useContext(MoviesContext);
   return (
-    <div style={{backgroundImage:`url(${featured && getImg(featured.backdrop_path)})`, backgroundSize:"cover",overflow:"hidden"}} className={`h-screen`}>
-      <NavBar />
-      <Outlet />
-    </div>
+    <>
+      {!toogleMovie ? (
+        <div
+          style={{
+            backgroundImage: `url(${
+              featured && getImg(featured.backdrop_path)
+            })`,
+            backgroundSize: "cover",
+            overflow: "hidden",
+          }}
+          className={`h-screen`}
+        >
+          <NavBar />
+          <Outlet />
+        </div>
+      ) : (
+        <div
+          className={`h-screen`}
+        >
+          <NavBar />
+          <Outlet />
+        </div>
+      )}
+    </>
   );
 };
 
