539e2072e138071b36aec0a1ef8e02e60d6257d5
Marianopiv <marianopividori93@gmail.com>
2023-03-27 00:07:38 -0300

    agrego mas random en featured

--- src/context/MoviesProvider.jsx
+++ src/context/MoviesProvider.jsx
@@ -24,7 +24,7 @@ const MoviesProvider = ({ children }) => {
 
   const fetchFeatured = async () => {
     setLoading(true);
-    const resultado = await get(`/popular${import.meta.env.VITE_API_KEY}&page=${Math.floor(Math.random()*4)}&language=${LANGUAGE}`);
+    const resultado = await get(`/popular${import.meta.env.VITE_API_KEY}&page=${Math.floor(Math.random()*8)}&language=${LANGUAGE}`);
     console.log(resultado)
     if (resultado) {
       const destacadoFinal = chooseMovie(resultado.results);
--- src/helper/index.js
+++ src/helper/index.js
@@ -2,7 +2,7 @@ import { API_IMG_PREFIX__HD } from "../config/config"
 
 export const getImg = (img) => `${API_IMG_PREFIX__HD}${img}`
 
-export const chooseMovie = (arr) => arr[Math.floor(Math.random() * 20)]
+export const chooseMovie = (arr) => arr[Math.floor(Math.random() * 10)]
 
 export const slicePages = (arr, num1, num2) => {
     return arr.slice(num1, num2);
